8e9f47a1b8b0720cca3b84f11c7a573689c8fec7
Milos Subotic <milos.subotic.sm@gmail.com>
2019-02-01 16:41:19 +0100

    Newer Julia and RC.

--- install_everything_on_Ubuntu.sh
+++ install_everything_on_Ubuntu.sh
@@ -26,12 +26,12 @@ git config --global user.email "milos.subotic.sm@gmail.com"
 # Julia.
 
 wget https://julialang-s3.julialang.org/bin/linux/x64/0.6/julia-0.6.4-linux-x86_64.tar.gz
-wget https://julialang-s3.julialang.org/bin/linux/x64/1.0/julia-1.0.2-linux-x86_64.tar.gz
+wget https://julialang-s3.julialang.org/bin/linux/x64/1.0/julia-1.0.3-linux-x86_64.tar.gz
 
 mkdir -p ~/local/julia
 
 tar xfv julia-0.6.4-linux-x86_64.tar.gz -C ~/local/julia
-tar xfv julia-1.0.2-linux-x86_64.tar.gz -C ~/local/julia
+tar xfv julia-1.0.3-linux-x86_64.tar.gz -C ~/local/julia
 
 pushd ~/local/julia/
 mv julia-9d11f62bcb/ julia-0.6.4/
@@ -39,9 +39,43 @@ popd
 
 pushd ~/bin/
 ln -s ../local/julia/julia-0.6.4/bin/julia julia064
-ln -s ../local/julia/julia-1.0.2/bin/julia julia102
-ln -s julia102 julia
+ln -s ../local/julia/julia-1.0.3/bin/julia julia103
+ln -s julia103 julia
 popd
+
+F=~/.juliarc.jl
+mkdir -p $(dirname $F)
+cp $F $F.backup-$(date +%F-%T | sed 's/:/-/g')
+cat > $F << EOF
+push!(LOAD_PATH, ".")
+
+#if VERSION >= v"0.7"
+#    using Pkg
+#end
+#for pkg in ["Revise"]
+#    if !haskey(Pkg.installed(), pkg)
+#        Pkg.add(pkg)
+#    end
+#end
+#
+#using Revise
+EOF
+
+F=~/.julia/config/startup.jl
+mkdir -p $(dirname $F)
+cp $F $F.backup-$(date +%F-%T | sed 's/:/-/g')
+cat > $F << EOF
+push!(LOAD_PATH, ".")
+
+using Pkg
+for pkg in ["Revise"]
+    if !haskey(Pkg.installed(), pkg)
+        Pkg.add(pkg)
+    end
+end
+
+using Revise
+EOF
 
 ###############################################################################
 # Worker.
