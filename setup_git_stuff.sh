e7de95352dfa3b4e14ef1160d59aa783de972ef3
Milos Subotic <milos.subotic.sm@gmail.com>
2020-01-18 19:16:07 +0100

    Correction.

--- setup_git_stuff.sh
+++ setup_git_stuff.sh
@@ -5,11 +5,10 @@ git config --global user.email "milos.subotic.sm@gmail.com"
 # To solve problem with unicode file names.
 git config --global core.quotePath false
 
-cat >> ~/.bashrc << EOF
+cat >> ~/.bashrc << 'EOF'
 alias gsta='git status'
 alias gcm='git commit -m'
 alias gcam='git commit -a -m'
-alias gad='git add'
 function gl() {
 	git log -1 --skip=`expr $1 - 1`
 }
