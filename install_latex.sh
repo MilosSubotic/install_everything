3c726235e153cf788085d933ce8a258a2427ace6
Milos Subotic <milos.subotic.sm@gmail.com>
2020-08-21 16:28:01 +0200

    Correction for latex.

--- install_latex.sh
+++ install_latex.sh
@@ -6,5 +6,4 @@ sudo apt install -y hunspell-en-us hunspell-sr # hunspell-sh
 # Texmaker -> Options -> Configure Texmaker -> Editor -> Spelling dictionary
 # Select some *.dic file from: /usr/share/hunspell/
 
-apm install latex language-latex
-#apm install  latex-autocomplete
+apm install latex language-latex latex-autocomplete
