fa26890bfd6d666fc3dbb05f92256a67ac7c09f5
Milos Subotic <milos.subotic.sm@gmail.com>
2021-03-30 15:51:03 +0200

    Better install.

--- install_arduino.sh
+++ install_arduino.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-
-wget https://downloads.arduino.cc/arduino-1.8.10-linux64.tar.xz
-mkdir ~/local/arduino
-tar xfv arduino-1.8.10-linux64.tar.xz -C ~/local/arduino
-rm arduino-1.8.10-linux64.tar.xz
-pushd ~/local/arduino/arduino-1.8.10/
+VER=1.8.12
+wget https://downloads.arduino.cc/arduino-$VER-linux64.tar.xz
+mkdir -p ~/.local/opt/arduino/
+tar xfv *.tar.xz -C ~/.local/opt/arduino
+rm *.tar.xz
+pushd ~/.local/opt/arduino/arduino-$VER/
 sudo ./install.sh
 popd
+
+F="$HOME/.arduino15/preferences.txt"
+sed -i 's/editor.tabs.expand=true/editor.tabs.expand=false/g' "$F"
+sed -i 's/editor.tabs.size=2/editor.tabs.size=4/g' "$F"
