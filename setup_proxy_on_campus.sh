8240945400493b9bf4443994b5f52dbd68e33aec
Milos Subotic <milos.subotic.sm@gmail.com>
2021-04-12 22:34:07 +0200

    Proxy script.

---
+++ setup_proxy_on_campus.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+URL="proxy.uns.ac.rs"
+PORT="8080"
+
+F=~/tmp/etc/environment
+
+echo "http_proxy=\"http://$URL:$PORT/\"" >> $F
+echo "https_proxy=\"https://$URL:$PORT/\"" >> $F
+echo "ftp_proxy=\"ftp://$URL:$PORT/\"" >> $F
+echo "socks_proxy=\"socks://$URL:$PORT/\"" >> $F
+echo "no_proxy=\"localhost,127.0.0.1,::1\"" >> $F
+
