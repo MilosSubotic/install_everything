759572f39896ac56baaa710811e6f1edcb2fd8eb
Milos Subotic <milos.subotic.sm@gmail.com>
2022-04-06 16:58:57 +0200

    Corrections.

--- RPi/install_everything_on_campus.sh
+++ RPi/install_everything_on_campus.sh
@@ -3,4 +3,3 @@
 sudo cp proxy.sh /etc/profile.d/proxy.sh
 sudo cp 10proxy /etc/apt/apt.conf.d/10proxy
 sudo apt update
-/etc/network/interfaces
--- usbip_in_NTP510/server_RPi/install_in_NTP510.sh
+++ usbip_in_NTP510/server_RPi/install_in_NTP510.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 bash ../../RPi/install_everything_in_NTP510.sh
-bash install_usbip_client.sh
+bash install.sh
