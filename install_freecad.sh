28cb44967d37920b453d23bb17e534f3eca27d7b
Milos Subotic <milos.subotic.sm@gmail.com>
2023-01-10 18:46:05 +0100

    For proxy.

--- install_freecad.sh
+++ install_freecad.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo add-apt-repository -y ppa:freecad-maintainers/freecad-stable
+sudo -E add-apt-repository -y ppa:freecad-maintainers/freecad-stable
 sudo apt update
 sudo apt install -y freecad openscad
