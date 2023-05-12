540eea93209b0725157f66a753294e9f9d008b15
Milos Subotic <milos.subotic.sm@gmail.com>
2023-05-12 16:33:03 +0200

    New helper.

--- setup_git_stuff.sh
+++ setup_git_stuff.sh
@@ -22,4 +22,36 @@ function glsha() {
 function gld() {
 	git diff `glsha $1` `glsha $2` $3
 }
+function gdiff() {
+	if (( $# == 0 ))
+	then
+		git diff
+	else
+		if (( $1 == 0 ))
+		then
+			SHA1="--"
+		else
+			SHA1=`glsha $1`
+		fi
+		
+		if (( $# == 1 ))
+		then
+			git diff $SHA1
+		else
+			if (( $2 == 0 ))
+			then
+				SHA2="--"
+			else
+				SHA2=`glsha $2`
+			fi
+			
+			if (( $# == 2 ))
+			then
+				git diff $SHA1 $SHA2
+			else
+				git diff $SHA1 $SHA2 "$3"
+			fi
+		fi
+	fi
+}
 EOF
