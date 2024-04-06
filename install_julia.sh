7388f4eeca9c3da498d6bf24b9a9d9e2c5b3cf44
Milos Subotic <milos.subotic.sm@gmail.com>
2024-04-06 09:24:06 +0200

    Correction.

--- install_julia.sh
+++ install_julia.sh
@@ -6,7 +6,7 @@ V3=7
 B=64
 A=x86_64
 F=julia-$V1.$V2.$V3-linux-$A.tar.gz
-wget https://julialang-s3.julialang.org/bin/linux/x86/$V1.$V2/$F
+wget https://julialang-s3.julialang.org/bin/linux/x$B/$V1.$V2/$F
 
 mkdir -p ~/.local/opt/julia/$B
 
@@ -14,7 +14,7 @@ tar xfv $F -C ~/.local/opt/julia/$B
 
 mkdir -p ~/.local/bin/
 pushd ~/.local/bin/
-ln -sf ../opt/julia/$A/julia-$V1.$V2.$V3/bin/julia julia$B$V1$V2$V3
+ln -sf ../opt/julia/$B/julia-$V1.$V2.$V3/bin/julia julia$B$V1$V2$V3
 ln -sf julia$B$V1$V2$V3 julia
 popd
 
