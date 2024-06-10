f0ceeb73c53ed04cfd556c9df3199a01219e79c1
Milos Subotic <milos.subotic.sm@gmail.com>
2024-06-10 09:07:56 +0200

    Solution for ch340

--- additional_Ubuntu_22.sh
+++ additional_Ubuntu_22.sh
@@ -3,3 +3,11 @@
 sudo apt install nvidia-driver-550
 
 gsettings set org.freedesktop.ibus.panel.emoji hotkey "[]"
+
+# ch340 bug
+sudo rm -f /usr/lib/udev/rules.d/*brltty*.rules
+sudo udevadm control --reload-rules
+sudo systemctl stop brltty-udev.service
+sudo systemctl mask brltty-udev.service
+sudo systemctl stop brltty.service
+sudo systemctl disable brltty.service
