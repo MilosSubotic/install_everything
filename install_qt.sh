9725bb7466bb9cd7dfc8bf6912cd1463381eaac2
Milos Subotic <milos.subotic.sm@gmail.com>
2024-07-01 14:04:33 +0200

    Correction on Ubuntu 22

--- install_qt.sh
+++ install_qt.sh
@@ -3,8 +3,9 @@
 
 sudo apt -y install git build-essential cmake pkg-config
 sudo apt -y install libsqlite3-dev libsqlcipher-dev
-sudo apt -y install qt5-default qtcreator
+sudo apt -y install qtbase5-dev qt5-qmake qtcreator
 sudo apt -y install qttools5-dev-tools \
 	libqt5scintilla2-dev libqcustomplot-dev qttools5-dev
+sudo apt -y install libqt5serialbus5-dev libqt5serialport5-dev
 
 ###############################################################################
