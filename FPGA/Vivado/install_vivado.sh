22873dbfe7e82ac7e1297361da32bfc59fc9a6fa
Milos Subotic <milos.subotic.sm@gmail.com>
2024-08-06 17:50:31 +0200

    Comment

--- FPGA/Vivado/install_vivado.sh
+++ FPGA/Vivado/install_vivado.sh
@@ -7,7 +7,7 @@ V=2024.1
 # https://support.xilinx.com/s/question/0D52E00006hpmTmSAI/vivado-20183-final-processing-hangs-at-generating-installed-device-list-on-ubuntu-1904?language=en_US
 sudo apt -y install libtinfo5 libncurses5
 
-# Download from site.
+# Downloaded from site.
 sudo bash FPGAs_AdaptiveSoCs_Unified_${V}*_Lin64.bin 
 
 source /opt/Xilinx/Vivado/${V}/settings64.sh
