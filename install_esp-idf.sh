652a92afacd5f3c9f252ef51fa983a98561a4461
Milos Subotic <milos.subotic.sm@gmail.com>
2025-01-31 12:18:52 +0100

    Success installing.

--- install_esp-idf.sh
+++ install_esp-idf.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
+sudo apt -y install python3-venv
+
 sudo mkdir -p /opt/esp-idf/
 sudo chmod 777 /opt/esp-idf/
 
-git clone -b v4.3 --recursive https://github.com/espressif/esp-idf.git
-sudo mv esp-idf /opt/esp-idf/v4.3
+pushd /opt/esp-idf/
+git clone -b v5.4 --recursive https://github.com/espressif/esp-idf.git v5.4
 
-pushd /opt/esp-idf/v4.3/
-./install.sh
+export IDF_TOOLS_PATH=/opt/esp-idf/tools
+sed -i '2i\export IDF_TOOLS_PATH=/opt/esp-idf/tools\' /opt/esp-idf/v5.4/export.sh 
+
+pushd /opt/esp-idf/v5.4/
+./install.sh esp32c6
+popd
+
+
 popd
