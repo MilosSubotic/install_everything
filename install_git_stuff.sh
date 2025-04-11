cc79dfd00260e06e82be4e535798a4e0a98aa1e2
Milos Subotic <milos.subotic.sm@gmail.com>
2025-04-11 16:10:00 +0200

    TODO

--- install_git_stuff.sh
+++ install_git_stuff.sh
@@ -5,9 +5,11 @@ sudo apt -y install git
 
 ./setup_git_stuff.sh
 
+#FIXME This does not work over SSH?
 #sudo apt -y install libsecret-1-0 libsecret-1-dev
 #sudo make --directory=/usr/share/doc/git/contrib/credential/libsecret
 #git config --global credential.helper /usr/share/doc/git/contrib/credential/libsecret/git-credential-libsecret
+
 git config --global credential.helper cache
 
 ###############################################################################
