b234b32d4ebc57c5f6589359a52da9d9eef5d1ed
Milos Subotic <milos.subotic.sm@gmail.com>
2025-08-21 18:19:30 +0200

    W

--- FPGA/Vivado/install_vivado.sh
+++ FPGA/Vivado/install_vivado.sh
@@ -2,15 +2,29 @@
 
 exit 0
 
-V=2024.1
+#V=2024.1
+V=2025.1
 
 # https://support.xilinx.com/s/question/0D52E00006hpmTmSAI/vivado-20183-final-processing-hangs-at-generating-installed-device-list-on-ubuntu-1904?language=en_US
-sudo apt -y install libtinfo5 libncurses5
+if [[ "$V" == "2024.1" ]]
+then
+    sudo apt -y install libtinfo5 libncurses5
+else
+    sudo apt -y install libtinfo6 libncurses6
+fi
 
 # Downloaded from site.
-sudo bash FPGAs_AdaptiveSoCs_Unified_${V}*_Lin64.bin 
+sudo bash FPGAs_AdaptiveSoCs_Unified_*${V}*_Lin64.bin
+# Setup proxy
+# Vitis
+# 
 
-source /opt/Xilinx/Vivado/${V}/settings64.sh
+if [[ "$V" == "2024.1" ]]
+then
+    source /tools/Xilinx/Vivado/${V}/settings64.sh
+else
+    source /tools/Xilinx/${V}/Vivado/settings64.sh
+fi
 
 sudo $XILINX_VITIS/scripts/installLibs.sh
 
@@ -23,7 +37,7 @@ then
         sudo rm /etc/profile.d/proxy.sh
 
         #TODO Test this, espec with Vitis and install_brds.sh
-        sudo tee -a /opt/Xilinx/Vivado/${V}/settings64.sh << EOF
+        sudo tee -a /tools/Xilinx/Vivado/${V}/settings64.sh << EOF
 unset http_proxy
 unset https_proxy
 EOF
