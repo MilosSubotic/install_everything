cc66bb0381e3b01b8465610eb6580cbdac759df8
Milos Subotic <milos.subotic.sm@gmail.com>
2025-09-20 18:43:49 +0200

    Freecad

--- install_freecad.sh
+++ install_freecad.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-sudo -E add-apt-repository -y ppa:freecad-maintainers/freecad-stable
-sudo apt update
-sudo apt install -y freecad openscad
+#sudo -E add-apt-repository -y ppa:freecad-maintainers/freecad-stable
+#sudo apt update
+#sudo apt install -y freecad openscad1
+
+V=1.0.2
+
+wget https://github.com/FreeCAD/FreeCAD/releases/download/${V}/FreeCAD_${V}-conda-Linux-x86_64-py311.AppImage
+
+mkdir -p ~/.local/opt/FreeCad
+chmod a+x FreeCAD_${V}-*.AppImage
+mv FreeCAD_${V}*.AppImage ~/.local/opt/FreeCad/
+ln -sf ~/.local/opt/FreeCad/FreeCAD_${V}*.AppImage ~/.local/bin/freecad
